987658be46dee0001dc2b4cc391ade17843b0b0c
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-01 17:54:36 -0700

    rqg v3 css and js changes

--- fccprojects/js/randomQuoteGenerator2.js
+++ fccprojects/js/randomQuoteGenerator2.js
@@ -1,15 +1,56 @@
-var currQuote = "", currAuth = "";
-var quoteURL = "http://quotes.stormconsultancy.co.uk/quotes/random.json?callback=";
+var currQuote = "", currAuth = "", currCat = "";
+
+function setHeaderText() {
+  switch(currCat) {
+    case "Famous":
+      return currCat + " quotes";
+      break;
+    case "Movies":
+      return "movie quotes";
+      break;
+                }
+}
+
+function setTwitterHash() {
+  switch(currCat) {
+    case "Famous":
+      return "famousquotes";
+      break;
+    case "Movies":
+      return "moviequotes";
+      break;
+                }
+}
+
 function loadQuote() {
-  $.getJSON(quoteURL, function(quoteMeta) {
-    currQuote = quoteMeta.quote;
-    currAuth = quoteMeta.author;
-    $("#theQuote").html(currQuote);
-    $("#theAuthor").html(currAuth);
-    $("#tweetQuote").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&via=fentablar&text=' + encodeURIComponent('"' + currQuote + '" -' + currAuth));
+  $("body").animate({
+    opacity: 0
+  }, 375);
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "QJs3ZNm6xHmshsvyHooxiDvMmf2dp14jKOAjsn1VTpXzU8tlKv",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    },
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=",
+    success: function(quoteMeta) {
+      currQuote = JSON.parse(quoteMeta).quote;
+      currAuth = JSON.parse(quoteMeta).author;
+      currCat = JSON.parse(quoteMeta).category;
+      $("#headerText").html(setHeaderText);
+      $("#footerText").html("page by fentablar");
+      $("#theQuote").html('\u201C' + currQuote + '\u201D');
+      $("#theAuthor").html(currAuth);
+      $("#iterateQuote").html("iterate");
+      $("#tweetQuote").html("share via twitter");
+      $("#tweetLink").attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('\u201C' + currQuote + '\u201D ' + currAuth) + '&hashtags=' + setTwitterHash() + '&via=fentablar');
+      $("body").animate({
+        opacity: 1
+      }, 375);
+    }
   });
 }
-$(document).ready(function () {
+$(document).ready(function() {
   loadQuote();
-  $("#newQuote").on("click", loadQuote);
+  $("#iterateQuote").on("click", loadQuote);
 });
