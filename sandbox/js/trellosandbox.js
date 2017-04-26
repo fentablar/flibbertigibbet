b0c6b6e01816876b69be18a4d7bedcea2bf27e35
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-26 12:11:53 -0700

    modify trelloAuth to be global function var

--- sandbox/js/trellosandbox.js
+++ sandbox/js/trellosandbox.js
@@ -1,33 +1,24 @@
-var trelloAuthSuccess = function() {
-  console.log("authentication SUCCESS");
-};
-var trelloAuthFail = function() {
-  console.log("authentication FAIL");
-};
-/*
-Trello.authorize({
-  type: "popup",
-  name: "Getting Started Application",
-  scope: {
-    read: "true",
-    write: "true"
-  },
-  expiration: "never",
-  success: trelloAuthSuccess,
-  error: trelloAuthFail
-});
-*/
-$(document).ready(function() {
-  console.log("JavaScript loaded");
+function trelloAuth() {
+  var trelloAuthSuccess = function() {
+    console.log("Trello auth SUCCESS");
+  };
+  var trelloAuthFail = function() {
+    console.log("Trello auth FAIL");
+  };
   Trello.authorize({
     type: "popup",
     name: "Getting Started Application",
     scope: {
       read: "true",
-      write: "true"
+      write: "true",
     },
     expiration: "never",
     success: trelloAuthSuccess,
     error: trelloAuthFail
   });
+}
+
+$(document).ready(function() {
+  console.log("JavaScript loaded");
+  trelloAuth();
 });
