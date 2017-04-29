22805f6b024abcb76282ec9e0a6e056cb17e3ffe
Fentablar Keldister <fentablar.keldister@gmail.com>
2017-04-29 00:07:07 -0700

    testing holdready

---
+++ sandbox/js/trellosandbox2.js
@@ -0,0 +1,36 @@
+$.holdReady(true);
+
+var reapTrelloData = function() {
+  Trello.get("members/me/boards", function(boardData) {
+    for (var bd = 0; bd < boardData.length; bd++) {
+      console.log("board " + boardData[bd].id);
+      Trello.get("boards/" + boardData[bd].id + "/lists", function(listData) {
+        for (var ld = 0; ld < listData.length; ld++) {
+          console.log("list " + listData[ld].id);
+          Trello.get("lists/" + listData[ld].id + "/cards", function(cardData) {
+            for (var cd = 0; cd < cardData.length; cd++) {
+              console.log("card " + cardData[cd].id);
+            }
+          }, function() { console.log("card load failed"); });
+        }
+      }, function() { console.log("list load failed"); });
+    }
+  }, function() { console.log("board load failed"); });
+  return $.holdReady(false);
+};
+
+Trello.authorize( {
+  type: "popup",
+  name: "chingu raccoons xtal",
+  scope: {
+    read: "true",
+    write: "true"
+  },
+  expiration: "never",
+  success: reapTrelloData,
+  error: trelloAuthFail
+});
+
+$(document).ready(function() {
+  console.log("document ready");
+});
